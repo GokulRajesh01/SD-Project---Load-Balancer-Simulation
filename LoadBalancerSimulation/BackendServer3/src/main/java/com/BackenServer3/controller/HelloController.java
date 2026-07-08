ff00f1fe60d9535697be1c8b8af50162d796c2f8
Gokul Rajesh <goks3003@gmail.com>
2026-07-09 03:59:20 +0530

    Added reqCountHit endpoint to backend server controllers, Completed K6 Performance spike test and uploaded results as screenshots

--- LoadBalancerSimulation/BackendServer3/src/main/java/com/BackenServer3/controller/HelloController.java
+++ LoadBalancerSimulation/BackendServer3/src/main/java/com/BackenServer3/controller/HelloController.java
@@ -5,12 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 public class HelloController {
+    private final AtomicInteger reqCount = new AtomicInteger(0);
+
     @GetMapping("/api/v1/hello")
     public ResponseEntity<String> helloResponse(){
+        reqCount.incrementAndGet();
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("Hello, from server 3!");
+    }
+
+    @GetMapping("/count")
+    public String getReqCount(){
+        return "Server 3 Count : " + reqCount.get();
     }
 }
--- LoadBalancerSimulation/PerformanceTest/RoundRobin_k6Tests.js
+++ LoadBalancerSimulation/PerformanceTest/RoundRobin_k6Tests.js
@@ -1,1 +1,19 @@
 // K6 - Performance Test upcoming in this Space
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+    stages: [
+        { duration: '30s', target: 2000}, // ramp-up
+        { duration: '2m', target: 2000}, // stable 
+        { duration: '30s' , target: 0} // ramp-down
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<100'], // 99% of request must be completed
+    }
+};
+
+export default () => {
+    http.get('http://localhost:8080/api/v1/sayHello');
+    sleep(1);
+}
